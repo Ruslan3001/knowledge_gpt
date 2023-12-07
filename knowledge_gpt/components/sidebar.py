44e0a7f08cdf61d7ee8cd8b2d67c515f8a583cf2
Ruslan3001 <ruslan3001@mail.ru>
2023-12-08 02:21:54 +0300

    Update sidebar.py

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -39,72 +39,7 @@ def sidebar():
             value=1,
             help="Более высокое значение делает вывод более случайным"  # noqa: E501
         )   
-        st.markdown(
-            "Температура выборки, значения от 0 до 2. Более высокие значения, такие как 1.8,\n"
-            "сделают вывод более случайным, в то время как более низкие значения, такие как 0.2,\n"
-            "сделают его более целенаправленным и детерминированным.\n"
-            "Не рекомендуется использовать совместно с параметром top_p.\n"
-        )
-
-        # top_p:
-        par_top_p_input = st.text_input(
-            "top_p",
-            type="default",
-            placeholder="Значения от 0 до 1",
-            disabled=False,
-            label_visibility="visible",
-            value="",
-            help="Более высокое значение делает вывод более случайным"  # noqa: E501
-        )   
-        st.markdown(
-            "Тальтернатива параметру temperature, где модель учитывает результаты токенов с\n"
-            "вероятностной массой top_p. Таким образом, 0.1 означает, что учитываются только \n"
-            "токены, составляющие верхнюю 10%-ную массу вероятности. Значения от 0 до 1.\n"
-            "Не рекомендуется использовать совместно с параметром temperature.\n"
-        )
-        
-        #n :
-        par_n_input = st.text_input(
-            "N-число вариантов ответов",
-            type="default",
-            placeholder="Значения от 1 до 4",
-            disabled=False,
-            label_visibility="visible",
-            value="1",
-            help="Значения от 1 до 4"  # noqa: E501
-        )   
-        st.markdown(
-            "Число вариантов ответов модели, которые необходимо сгенерировать \n"
-            "для каждого входного сообщения. Максимально возможное значение n=4.\n"
-        )
-
-        #max_tokens :
-        par_max_tokens_input = st.text_input(
-            "max_tokens",
-            type="default",
-            placeholder="Значения ????",
-            disabled=False,
-            label_visibility="visible",
-            value="1",
-            help="Мальное количество токенов для генерации ответов"  # noqa: E501
-        )  
-
-        #repetition_penalty :
-        par_rep_penal_input = st.text_input(
-            "repetition_penalty",
-            type="default",
-            placeholder="Значения ????",
-            disabled=False,
-            label_visibility="visible",
-            value="1.0",
-            help="Количество повторений слов"  # noqa: E501
-        )  
-        st.markdown(
-            "Количество повторений слов: Значение 1.0 — ничего не менять (нейтральное значение),\n"
-            "от 0 до 1 — повторять уже сказанные слова, \n"
-            "от 1 и далее стараться не использовать сказанные слова.\n"
-        )
-       
+      
         St.markdown("[Описание API GigaChat](https://developers.sber.ru/docs/ru/gigachat/api/reference)")    
         
         st.markdown("---")
